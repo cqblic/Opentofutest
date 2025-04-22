3c5d2c27eaff3d283c9c05f4015df7e98d27731d
cqblic <carlos@blic.llc>
2025-04-22 16:44:54 -0700

    Update variables.tf
    define variables

--- variables.tf
+++ variables.tf
@@ -1,26 +1,16 @@
-variable "resource_group_name" {
-  description = "The name of the resource group in which to create the resources."
-  type        = string
-  default     = "tofuBLIC_rg"
+variable "prefix" {
+  type    = string
+  default = "tofu"
 }
-
 variable "location" {
-  description = "The location/region where the resources will be created."
-  type        = string
-  default     = "US West"
+  type    = string
+  default = "westus2"
 }
-
-variable "vm_size" {
-  description = "The size of the Virtual Machines."
-  type        = string
-  default     = "Standard_B2s"
+variable "rg_name" {
+  type    = string
+  default = "OHIA-CarlosQuintanar-SBOX-RG"
 }
-
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "tofuBLIC_rg" {
-  name     = var.resource_group_name
-  location = var.location
+variable "vm_name" {
+  type    = string
+  default = "nistApp"
 }
