96164f89236d991255f69918a846accf4372ad37
cqblic <carlos@blic.llc>
2025-04-22 16:44:19 -0700

    Update network.tf
    define network vnet, nsg, and subnets

--- network.tf
+++ network.tf
@@ -1,20 +1,42 @@
-resource "azurerm_virtual_network" "tofuBLIC_VNet" {
-  name                = "tofuBLIC-vnet"
-  resource_group_name = azurerm_resource_group.tofuBLIC_rg.name
-  location            = azurerm_resource_group.tofuBLIC_rg.location
-  address_space       = ["10.0.0.0/16"]
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-vnet"
+  address_space       = ["10.1.0.0/16"]
+  location            = var.location
+  resource_group_name = var.rg_name
 }
 
-resource "azurerm_subnet" "tofuBLIC_Web_Subnet" {
-  name                 = "tofuBLIC-web-subnet"
-  resource_group_name  = azurerm_resource_group.tofuBLIC_rg.name
-  virtual_network_name = azurerm_virtual_network.tofuBLIC_VNet.name
-  address_prefixes     = ["10.0.1.0/24"]
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = var.rg_name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.1.2.0/24"]
 }
 
-resource "azurerm_subnet" "tofuBLIC_App_Subnet" {
-  name                 = "tofuBLIC-app-subnet"
-  resource_group_name  = azurerm_resource_group.tofuBLIC_rg.name
-  virtual_network_name = azurerm_virtual_network.tofuBLIC_VNet.name
-  address_prefixes     = ["10.0.2.0/24"]
+resource "azurerm_network_security_group" "main" {
+  name                = "${var.prefix}-nsg"
+  location            = var.location
+  resource_group_name = var.rg_name
+  security_rule {
+    name                       = "allow-ssh"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                    = "Allow"
+    protocol                  = "Tcp"
+    source_port_range         = "*"
+    destination_port_range     = 22
+    source_address_prefix     = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = var.location 
+  resource_group_name = var.rg_name
+  
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
 }
